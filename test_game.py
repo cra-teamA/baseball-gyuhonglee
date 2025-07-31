5002e81e3703f7a5e917e4fa53560f11eb27bc8f
gyuhong <jagijudo@gmail.com>
2025-07-31 14:00:49 +0900

    [refactor] p38

--- test_game.py
+++ test_game.py
@@ -12,11 +12,9 @@ def assert_illegal_argument(game, guessNumber):
     except TypeError:
         pass
 
-def test_exception_when_input_is_none(game):
+def test_exception_when_invalid_input(game):
     assert_illegal_argument(game, None)
-
-
-def test_exception_when_input_length_is_unmatched(game):
     assert_illegal_argument(game, "12")
+    assert_illegal_argument(game, "1234")
 
 
